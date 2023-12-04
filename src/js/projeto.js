ccd98ebaf5dad5a2b19f88a5f6ea60a47c2e711b
Maiara Viegas <111406385+DevMaiWolf@users.noreply.github.com>
2023-12-04 09:28:16 -0300

    commit -m "mais ajustes"

--- src/js/projeto.js
+++ src/js/projeto.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     breakpoints: {
       360: {
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 10.7,
       },
       480: {
         slidesPerView: 1,
